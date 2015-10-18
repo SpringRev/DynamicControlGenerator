23112ef1dcd2e7607edef5cbdb2826627691c92d
SpringRev <revathiscareer@gmail.com>
2015-10-18 16:53:22 -0400

    Dynamic Control
    Dynamic Control  Generation

---
+++ DynamicControlGenerator/Models/AdditionalField.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicControlGenerator.Models
+{
+    public class AdditionalField
+    {
+        public string controlName { get; set; }
+        public string controlType { get; set; }
+    }
+}
---
+++ DynamicControlGenerator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DynamicControlGenerator.Startup))]
+namespace DynamicControlGenerator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
